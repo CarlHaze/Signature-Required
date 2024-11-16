3a638e83456e40c5cbf24dba160da21ec8a2ff88
Carl Ellis <carlhlellis89@gmail.com>
2024-11-16 18:46:23 +1300

    basic animations done time to do something with them...

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,6 +8,16 @@ public class Player : MonoBehaviour
     public int Money = 0; // Total money acquired 
     public int TotalPackagesDelivered = 0; // lifetime Packages delivered.
 
+    // Player Health and Stamina
+    public int maxHealth = 100;
+    public int Health;
+    public int Stamina = 100;
+
+    // Player Strength and Damage
+    public int Strength = 10;
+    public int Damage = 10;
+
+
     private void Awake()
     {
         if (Instance == null)
@@ -18,6 +28,9 @@ public class Player : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        Health = maxHealth;
+
     }
 
     public void IncrementTotalPackagesDelivered()
